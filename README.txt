659a34bd35126469bc8ecc3b95c0cfc0b5893c81
Jon-Morten Kristiansen <jon.morten.kristiansen@gmail.com>
2012-03-10 23:33:32 +0100

    Elements get a class when scaled. Also some refactoring done, using native JS when possible

--- README.txt
+++ README.txt
@@ -9,5 +9,4 @@ TODO
 * Browser compatibility testing
 * Optimize
 * Features
-    - add a class to images that are scaled, to allow styling, e.g. cursor to indicate togglability
     - consider adding option for a lightboxish overlay behind images when toggled to original size, to make it more obvious what is going on
--- jquery.scaletoviewport.js
+++ jquery.scaletoviewport.js
@@ -1,5 +1,5 @@
 /*
-*   jQuery Scale To Viewport plugin v1.1
+*   jQuery Scale To Viewport plugin v1.2
 */
 
 ( function( $ ) {
@@ -9,35 +9,53 @@
         var settings = $.extend( {
             'click_toggle_anchor': false,
             'click_toggle_scaling': true,
-            'css_toggle_class': 'original-size',
+            'css_scaled_class': 'stv-scaled',
+            'css_toggle_class': 'stv-original',
             'min_height': 300,
             'padding': 0
         }, options );
 
         return this.each( function() {
 
-            var $window = $( window );
-            var $this = $( this );
-            var this_height;
+            var $viewport = $( window );
             var viewport_padding = settings.padding;
-            var this_min_height = settings.min_height;
 
-            $window.bind( 'load', function() {
+            var _this = this;
+            var $this = $( _this );
+            var this_offset_height; // Is set on load, to ensure elements have their actual height
+            var this_client_height; // Is set on load, to ensure elements have their actual height
+            var this_min_height = settings.min_height; // To add possible padding and border, this is modified on load
+            var has_scaled_class = false;
+            var scaled_class = [ ' ', settings.css_scaled_class ].join( '' );
 
-                this_height = $this.height();
-
-            } ).bind( 'resize load', function() {
-
-                var viewport_height = $window.height() - viewport_padding;
-
-                if ( viewport_height < this_height ) {
-                    $this.height( ( viewport_height > this_min_height ? viewport_height : this_min_height ) );
-                } else if ( $this.height() !== this_height ) {
-                    $this.height( this_height );
+            var scaleThisHeight = function( viewport_height ) {
+                viewport_height -= viewport_padding;
+                if ( viewport_height < this_offset_height ) {
+                    _this.style.height = [ ( viewport_height > this_min_height ? viewport_height : this_min_height ), 'px' ].join( '' );
+                    if ( !has_scaled_class ) {
+                        has_scaled_class = true;
+                        _this.className += scaled_class;
+                    }
+                } else if ( _this.offsetHeight !== this_offset_height ) {
+                    _this.style.height = [ this_client_height, 'px' ].join( '' );
+                    if ( has_scaled_class ) {
+                        has_scaled_class = false;
+                        _this.className = _this.className.replace( scaled_class, '' );
+                    }
                 }
+            };
 
+            // Events
+            $viewport.bind( 'load', function() {
+                this_offset_height = _this.offsetHeight;
+                this_client_height = _this.clientHeight;
+                this_min_height += ( this_offset_height - this_client_height );
+                scaleThisHeight( $viewport.height() );
+            } ).bind( 'resize', function() {
+                scaleThisHeight( $viewport.height() );
             } );
 
+            // Scale toggle
             if ( settings.click_toggle_scaling ) {
                 ( settings.click_toggle_anchor || $this ).bind( 'click', function() {
                     $this.toggleClass( settings.css_toggle_class );
