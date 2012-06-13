c13b233f8936193646a5d5eec55838a8e45c6c8b
Jon-Morten Kristiansen <jon.morten.kristiansen@gmail.com>
2012-06-13 22:57:42 +0200

    Naming cleanup.

--- jquery.scaletoviewport.js
+++ jquery.scaletoviewport.js
@@ -1,6 +1,6 @@
-/*
-*   jQuery Scale To Viewport plugin v1.2
-*/
+//
+// jQuery Scale To Viewport plugin v1.3
+//
 
 ( function( $ ) {
 
@@ -20,45 +20,46 @@
             var $viewport = $( window );
             var viewport_padding = settings.padding;
 
-            var _this = this;
-            var $this = $( _this );
-            var this_offset_height; // Is set on load, to ensure elements have their actual height
-            var this_client_height; // Is set on load, to ensure elements have their actual height
-            var this_min_height = settings.min_height; // To add possible padding and border, this is modified on load
+            var _elem = this;
+            var $elem = $( _elem );
+            var elem_offset_height; // Is set on load, to ensure elements have their actual height
+            var elem_client_height; // Is set on load, to ensure elements have their actual height
+            var elem_min_height = settings.min_height; // To add possible padding and border, this is modified on load
             var has_scaled_class = false;
             var scaled_class = [ ' ', settings.css_scaled_class ].join( '' );
 
-            var scaleThisHeight = function( viewport_height ) {
+            // Scale function
+            var scaleElemHeight = function( viewport_height ) {
                 viewport_height -= viewport_padding;
-                if ( viewport_height < this_offset_height ) {
-                    _this.style.height = [ ( viewport_height > this_min_height ? viewport_height : this_min_height ), 'px' ].join( '' );
+                if ( viewport_height < elem_offset_height ) {
+                    _elem.style.height = [ ( viewport_height > elem_min_height ? viewport_height : elem_min_height ), 'px' ].join( '' );
                     if ( !has_scaled_class ) {
                         has_scaled_class = true;
-                        _this.className += scaled_class;
+                        _elem.className += scaled_class;
                     }
-                } else if ( _this.offsetHeight !== this_offset_height ) {
-                    _this.style.height = [ this_client_height, 'px' ].join( '' );
+                } else if ( _elem.offsetHeight !== elem_offset_height ) {
+                    _elem.style.height = [ elem_client_height, 'px' ].join( '' );
                     if ( has_scaled_class ) {
                         has_scaled_class = false;
-                        _this.className = _this.className.replace( scaled_class, '' );
+                        _elem.className = _elem.className.replace( scaled_class, '' );
                     }
                 }
             };
 
             // Events
             $viewport.bind( 'load', function() {
-                this_offset_height = _this.offsetHeight;
-                this_client_height = _this.clientHeight;
-                this_min_height += ( this_offset_height - this_client_height );
-                scaleThisHeight( $viewport.height() );
+                elem_offset_height = _elem.offsetHeight;
+                elem_client_height = _elem.clientHeight;
+                elem_min_height += ( elem_offset_height - elem_client_height );
+                scaleElemHeight( $viewport.height() );
             } ).bind( 'resize', function() {
-                scaleThisHeight( $viewport.height() );
+                scaleElemHeight( $viewport.height() );
             } );
 
             // Scale toggle
             if ( settings.click_toggle_scaling ) {
-                ( settings.click_toggle_anchor || $this ).bind( 'click', function() {
-                    $this.toggleClass( settings.css_toggle_class );
+                ( settings.click_toggle_anchor || $elem ).bind( 'click', function() {
+                    $elem.toggleClass( settings.css_toggle_class );
                 } );
             }
 
