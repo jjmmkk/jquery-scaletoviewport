d41ff6b106f525c3c0290968c08f891614ba19b0
Jon-Morten Kristiansen <jon.morten.kristiansen@gmail.com>
2012-06-17 18:31:04 +0200

    Added destroy method and possibility to throttling the scaling.

--- README.md
+++ README.md
@@ -5,10 +5,27 @@
 Scales elements to keep their height less than that of the viewport.
 
 
+## Usage
+
+Include the script and use ``scaleToViewport()`` with a jQuery selector.
+
+    $('img').scaleToViewport();
+
+### Options
+
+    $('img').scaleToViewport( {
+        'click_toggle_anchor': boolean || false,
+        'click_toggle_scaling': boolean || true,
+        'css_scaled_class': string || 'stv-scaled',
+        'css_toggle_class': string || 'stv-original',
+        'min_height': number || 300,
+        'padding': number || 0,
+        'throttle': number || 0
+    } );
+
 ## Todo
 
-* Documentation
 * Browser compatibility testing
-* Optimize
-* Features
-    * consider adding option for a lightboxish overlay behind images when toggled to original size, to make it more obvious what is going on
+* Add support for elements that
+    * Were hidden on load
+    * Change their actual height after load
--- jquery.scaletoviewport.js
+++ jquery.scaletoviewport.js
@@ -1,69 +1,121 @@
 //
-// jQuery Scale To Viewport plugin v1.3
+// jQuery Scale To Viewport plugin v1.4
 //
 
-( function( $ ) {
+( function ( $ ) {
 
-    $.fn.scaleToViewport = function( options ) {
+    // Throttle
+    function throttle ( fn, delay ) {
+        var timer = null;
+        return function () {
+            var context = this;
+            var args = arguments;
+            clearTimeout( timer );
+            timer = setTimeout( function () {
+                fn.apply( context, args );
+            }, delay );
+        };
+    }
 
-        var settings = $.extend( {
-            'click_toggle_anchor': false,
-            'click_toggle_scaling': true,
-            'css_scaled_class': 'stv-scaled',
-            'css_toggle_class': 'stv-original',
-            'min_height': 300,
-            'padding': 0
-        }, options );
+    // Cache viewport
+    var $viewport = $( window );
 
-        return this.each( function() {
+    // Methods
+    var methods = {
 
-            var $viewport = $( window );
-            var viewport_padding = settings.padding;
+        init: function ( options ) {
+            var settings = $.extend( {
+                'click_toggle_anchor': false,
+                'click_toggle_scaling': true,
+                'css_scaled_class': 'stv-scaled',
+                'css_toggle_class': 'stv-original',
+                'min_height': 300,
+                'padding': 0,
+                'throttle': 0
+            }, options );
+            console.log(settings.throttle);
 
-            var _elem = this;
-            var $elem = $( _elem );
-            var elem_offset_height; // Is set on load, to ensure elements have their actual height
-            var elem_client_height; // Is set on load, to ensure elements have their actual height
-            var elem_min_height = settings.min_height; // To add possible padding and border, this is modified on load
-            var has_scaled_class = false;
-            var scaled_class = [ ' ', settings.css_scaled_class ].join( '' );
+            return this.each( function () {
 
-            // Scale function
-            var scaleElemHeight = function( viewport_height ) {
-                viewport_height -= viewport_padding;
-                if ( viewport_height < elem_offset_height ) {
-                    _elem.style.height = [ ( viewport_height > elem_min_height ? viewport_height : elem_min_height ), 'px' ].join( '' );
-                    if ( !has_scaled_class ) {
-                        has_scaled_class = true;
-                        _elem.className += scaled_class;
+                var viewport_padding = settings.padding;
+
+                var elem = this;
+                var $elem = $( elem );
+                var elem_offset_height;// To ensure elements have their actual height, this is set on load
+                var elem_client_height;// To ensure elements have their actual height, this is set on load
+                var elem_min_height = settings.min_height;// To add possible padding and border, this is modified on load
+                var has_scaled_class = false;
+                var scaled_class = [ ' ', settings.css_scaled_class ].join( '' );
+
+                // Scale function
+                var scaleElemHeight = function ( viewport_height ) {
+                    viewport_height -= viewport_padding;
+                    if ( viewport_height < elem_offset_height ) {
+                        elem.style.height = [ ( viewport_height > elem_min_height ? viewport_height : elem_min_height ), 'px' ].join( '' );
+                        if ( !has_scaled_class ) {
+                            has_scaled_class = true;
+                            elem.className += scaled_class;
+                        }
+                    } else if ( elem.offsetHeight !== elem_offset_height ) {
+                        elem.style.height = [ elem_client_height, 'px' ].join( '' );
+                        if ( has_scaled_class ) {
+                            has_scaled_class = false;
+                            elem.className = elem.className.replace( scaled_class, '' );
+                        }
                     }
-                } else if ( _elem.offsetHeight !== elem_offset_height ) {
-                    _elem.style.height = [ elem_client_height, 'px' ].join( '' );
-                    if ( has_scaled_class ) {
-                        has_scaled_class = false;
-                        _elem.className = _elem.className.replace( scaled_class, '' );
-                    }
+                };
+
+                // Events
+                $viewport.on( 'load.scaleToViewport', function () {
+                    elem_offset_height = elem.offsetHeight;
+                    elem_client_height = elem.clientHeight;
+                    elem_min_height += ( elem_offset_height - elem_client_height );
+                    scaleElemHeight( $viewport.height() );
+                } ).on( 'resize.scaleToViewport',
+                    (
+                        ( settings.throttle > 0 ) ?
+                        throttle( function () {
+                            scaleElemHeight( $viewport.height() );
+                        }, settings.throttle )
+                        :
+                        function () {
+                            scaleElemHeight( $viewport.height() );
+                        }
+                    )
+                );
+
+                // Scale toggle
+                if ( settings.click_toggle_scaling ) {
+                    ( settings.click_toggle_anchor || $elem ).bind( 'click', function () {
+                        $elem.toggleClass( settings.css_toggle_class );
+                    } );
                 }
-            };
 
-            // Events
-            $viewport.bind( 'load', function() {
-                elem_offset_height = _elem.offsetHeight;
-                elem_client_height = _elem.clientHeight;
-                elem_min_height += ( elem_offset_height - elem_client_height );
-                scaleElemHeight( $viewport.height() );
-            } ).bind( 'resize', function() {
-                scaleElemHeight( $viewport.height() );
             } );
+        },
 
-            // Scale toggle
-            if ( settings.click_toggle_scaling ) {
-                ( settings.click_toggle_anchor || $elem ).bind( 'click', function() {
-                    $elem.toggleClass( settings.css_toggle_class );
-                } );
-            }
+        destroy: function () {
+            $viewport.off( '.scaleToViewport' );
+            return this;
+        }
 
-        } );
+    };
+
+    // Add to jQuery
+    $.fn.scaleToViewport = function ( method ) {
+
+        // Method exists
+        if ( methods[method] ) {
+            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ) );
+        }
+        // Initialize
+        else if ( typeof method === 'object' || !method ) {
+            return methods.init.apply( this, arguments );
+        }
+        // Error
+        else {
+            throw 'scaleToViewport() does not have the method "' + method + '"';
+        }
 
     };
 
